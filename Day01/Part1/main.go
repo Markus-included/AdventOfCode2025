1b6729cce79344410cc2b6ff804083b74859623f
Markus Lindner <markus.lindner@it.htl-hl.ac.at>
2025-12-01 12:04:50 +0100

    Day01 solution, Day02 half-working solution which works on the small example input but not the real one

---
+++ Day01/Part1/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("inputs/day01.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	input := string(data)
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+
+	fmt.Println("Part 1")
+	fmt.Println("Lines:", len(lines))
+
+	dial := 50
+	zeroPositions := 0
+
+	for lineNum, line := range lines {
+		sign := 1
+		if line[0] == 'L' {
+			sign = -1
+		}
+		i, err := strconv.Atoi(line[1:])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to parse line: ", lineNum, " ", line)
+		}
+		dial = (dial + (sign * i)) % 100
+
+		if dial == 0 {
+			zeroPositions += 1
+		}
+	}
+
+	fmt.Println("Passcode: ", zeroPositions)
+}
