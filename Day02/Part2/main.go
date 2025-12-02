85c22edef7079b596de1a57b7a9106f133b227b6
Markus Lindner <markus.lindner@it.htl-hl.ac.at>
2025-12-02 11:19:55 +0100

    Day02 Part 1

---
+++ Day02/Part2/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"AdventOfCode2025/mprofiler"
+	"fmt"
+	"os"
+	"runtime/debug"
+	"strconv"
+	"strings"
+)
+
+func Atoi64(s string) (int64, error) {
+	return strconv.ParseInt(s, 10, 64)
+}
+
+func main() {
+	data, err := os.ReadFile("inputs/day02.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	input := string(data)
+	rows := strings.Split(strings.TrimSpace(input), ",")
+
+	var ids []int64
+
+	for _, row := range rows {
+		rang := strings.Split(row, "-")
+
+		start, _ := Atoi64(rang[0])
+		end, _ := Atoi64(rang[1])
+
+		for i := start; i <= end; i++ {
+			// I am well aware that this is hella slow, TODO: optimize
+			ids = append(ids, i)
+		}
+
+	}
+
+	fmt.Println("Part 2")
+	fmt.Println("IDs:", len(ids))
+
+	debug.SetGCPercent(-1)
+	defer mprofiler.TrackExecutionTime("Day02Part1")()
+
+	var sum int64 = 0
+
+	for _, id := range ids {
+		strId := strconv.FormatInt(id, 10)
+		if len(strId)%2 != 0 {
+			continue
+		}
+		if strId[:len(strId)/2] == strId[(len(strId)/2):] {
+			fmt.Println(strId[:len(strId)/2], strId[(len(strId)/2):])
+			sum += id
+		}
+	}
+
+	fmt.Println("Passcode: ", sum)
+}
--- mprofiler/profiler.go
+++ mprofiler/profiler.go
@@ -2,6 +2,7 @@ package mprofiler
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
@@ -10,5 +11,12 @@ func TrackExecutionTime(name string) func() {
 	return func() {
 		elapsed := time.Since(start)
 		fmt.Printf("[%s] took %s\n", name, elapsed)
+
+		var m runtime.MemStats
+		runtime.ReadMemStats(&m)
+		fmt.Printf("Alloc = %d bytes\n", m.Alloc)
+		fmt.Printf("TotalAlloc = %d bytes\n", m.TotalAlloc)
+		fmt.Printf("Sys = %d bytes\n", m.Sys)
+		fmt.Printf("NumGC = %d\n", m.NumGC)
 	}
 }
