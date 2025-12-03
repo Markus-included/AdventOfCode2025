38b1d422a7c70ae0cd10aa7c4721c1f3ce0efb7b
Markus Lindner <markus.lindner@it.htl-hl.ac.at>
2025-12-03 15:36:24 +0100

    Day 3 Part 2

--- Day03/Part2/main.go
+++ Day03/Part2/main.go
@@ -18,27 +18,24 @@ func main() {
 	debug.SetMemoryLimit(4 * 1024 * 1024 * 1024)
 	defer mprofiler.TrackExecutionTime("Day03Part2")()
 
-	totalJoltage := 0
+	var totalJoltage int64 = 0
 
 	for _, line := range input {
 
 		const DIGITS int = 12
 
-		joltage := 0
-
-		windowStart :=
-		windowEnd := len(line) -
-
+		var joltage int64 = 0
+		windowStart := 0
+		windowEnd := len(line) - DIGITS + 1
 		for i := 0; i < DIGITS; i++ {
-			requiredRest := DIGITS - 1 - i
-			search_end_pos := len(line) - requiredRest
-			largest := common.LargestIdxInArray()
-			fmt.Println(rest)
-			rest = rest[largest+1:]
-			joltage += int(math.Pow10(i))
+			slice := line[windowStart:min(windowEnd, len(line))]
+			relIdx := common.LargestIdxInArray(slice)
+			joltage += int64(math.Pow10(DIGITS-i-1)) * int64(slice[relIdx])
+			windowStart += relIdx + 1
+			windowEnd++
 		}
 
-		totalJoltage += int(joltage)
+		totalJoltage += int64(joltage)
 	}
 
 	fmt.Println("Total joltage: ", totalJoltage)
